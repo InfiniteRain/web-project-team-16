f49ecef29626e40db0337cae8f1520163066f5b2
Xanthe Hammer <augustqhammer@gmail.com>
2018-04-04 16:36:10 +0300

    Change file structure and add DB connection

---
+++ public/php/dbconnect.php
@@ -0,0 +1,28 @@
+<?php
+
+function db_connect() {
+
+    static $connection;
+
+    if(!isset($connection)) {
+        $config = parse_ini_file('../private/config.ini'); 
+        $connection = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
+    }
+
+    if($connection === false) {
+        echo "Database Connection Failed.";
+    }
+    return $connection;
+}
+
+// Connect to the database
+$connection = db_connect();
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+
+}
+
+
+?> 
