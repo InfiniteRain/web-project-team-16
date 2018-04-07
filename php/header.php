af889e873414be1b70f4fa57c6c9bee128c73b4b
infiniterain <lysenkodavid@gmail.com>
2018-04-07 18:46:44 +0300

    action.php for request handling; basic login/registration forms

--- php/header.php
+++ php/header.php
@@ -6,12 +6,8 @@ session_start();
 // Some constant definitions.
 define('CONFIG', parse_ini_file('../config.ini'));
 
-// TODO: REMOVE (FOR NOW ITS HERE FOR TESTING PURPOSES)
-$_SESSION['userId'] = 1;
-
 // Necessary includes.
 require_once __DIR__ . '/classes/Database.php';
 require_once __DIR__ . '/classes/Session.php';
 
 require_once __DIR__ . '/classes/models/User.php';
-
---
+++ public/action.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . '/../php/header.php';
+
+switch ($_POST['action']) {
+    case 'register':
+        try {
+            $newUser = new User();
+            $newUser->username = $_POST['username'];
+            $newUser->password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $newUser->first_name = $_POST['firstName'];
+            $newUser->last_name = $_POST['lastName'];
+            $newUser->email = $_POST['email'];
+            $newUser->type = 1;
+            $newUser->save();
+        } catch (Exception $exception) {
+
+        }
+
+        break;
+    case 'login':
+        try {
+            $user = User::where('username=?', [$_POST['username']]);
+
+            if (!isset($user[0])) {
+                break;
+            }
+
+            $user = $user[0];
+            if (!password_verify($_POST['password'], $user->password)) {
+                break;
+            }
+
+            Session::login($user);
+        } catch (Exception $exception) {
+
+        }
+
+        break;
+    case 'logout';
+        try {
+            Session::logout();
+        } catch (Exception $exception) {
+
+        }
+
+        break;
+    default:
+
+        break;
+}
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+die();
