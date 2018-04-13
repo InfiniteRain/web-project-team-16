9cfc618c3d4bdf629172d1d6eab61db283f2bcbe
infiniterain <lysenkodavid@gmail.com>
2018-04-13 12:30:05 +0300

    didnt commit properly last time

---
+++ bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+
+// Starting the session.
+session_start();
+
+// Some constant definitions.
+define('CONFIG', parse_ini_file('../config.ini'));
--- public/index.php
+++ public/index.php
@@ -1,56 +1,18 @@
 <?php
 
-require_once __DIR__ . '/../app/header.php';
+require_once __DIR__ . '/../bootstrap/autoload.php';
 
-use WebTech\Hospital\Session;
+require_once __DIR__ . '/../bootstrap/app.php';
 
+try {
+    \WebTech\Hospital\Router::handle();
+} catch (\Exception $exception) {
+    $stackTrace = preg_replace('/\n/', '<br>', $exception->getTraceAsString());
 
-
-?>
-
-<?php if (!Session::user()): ?>
-    <h1>User is not logged in</h1>
-    <h2>Registration</h2>
-    <form action="action.php" method="post">
-        <input type="hidden" name="action" value="register">
-
-        First name:<br>
-        <input type="text" name="firstName"><br>
-        Last name:<br>
-        <input type="text" name="lastName"><br>
-        E-Mail address:<br>
-        <input type="email" name="email"><br>
-        Username:<br>
-        <input type="text" name="username"><br>
-        Password:<br>
-        <input type="text" name="password"><br>
-        <br>
-        <button type="submit">Submit</button>
-    </form>
-    <hr>
-    <h2>Login</h2>
-    <form action="action.php" method="post">
-        <input type="hidden" name="action" value="login">
-
-        First name:<br>
-        <input type="text" name="username"><br>
-        Password:<br>
-        <input type="text" name="password"><br>
-        <br>
-        <button type="submit">Submit</button>
-    </form>
-<?php else: ?>
-    <h1>User is logged in</h1>
-    <h2>Login info:</h2>
-    Username: <b><?= Session::user()->username ?></b><br>
-    First name: <b><?= Session::user()->first_name ?></b><br>
-    Last name: <b><?= Session::user()->last_name ?></b><br>
-    <hr>
-    <h2>Logout</h2>
-    <form action="action.php" method="post">
-        <input type="hidden" name="action" value="logout">
-
-        <button type="submit">Logout</button>
-    </form>
-<?php endif; ?>
-
+    echo "
+        <h1>Uncaught exception: " . get_class($exception) . "</h1>
+        <h2>Message: {$exception->getMessage()}</h2>
+        <h3>Stacktrace:</h3>
+        <b>{$stackTrace}</b>
+    ";
+}
