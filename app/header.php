3476195642e15932c7ece3a59e31762dd0abe788
infiniterain <lysenkodavid@gmail.com>
2018-04-13 12:25:51 +0300

    implement Controller, Router classes, implement views; implement routing

--- app/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../bootstrap/init.php';
-require_once __DIR__ . '/../bootstrap/autoload.php';
--- bootstrap/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Starting the session.
-session_start();
-
-// Some constant definitions.
-define('CONFIG', parse_ini_file('../config.ini'));
