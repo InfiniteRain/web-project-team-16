70854f6a9807c4d31f03cc43847212acd74e6da9
maxkandzia <GIT SERVICE EMAIL>
2018-04-30 19:41:51 +0300

    update index.html

--- public/assets/frontend/js/main.js
+++ public/assets/frontend/js/main.js
@@ -1,17 +1,15 @@
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-showDivs(slideIndex += n);
-}
+var slideIndex = 0;
+showSlides();
 
-function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("mySlides");
-if (n > x.length) {slideIndex = 1} 
-if (n < 1) {slideIndex = x.length}
-for (i = 0; i < x.length; i++) {
-x[i].style.display = "none"; 
-}
-x[slideIndex-1].style.display = "block"; 
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
